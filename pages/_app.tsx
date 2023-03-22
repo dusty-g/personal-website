91037cdfdd2a6196b01eb9c4b63a8952dae53011
Dusty <dustygalindo@gmail.com>
2023-03-22 14:12:38 -0700

    add firebase to job-search-log

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,32 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// add database
+import { getDatabase } from "firebase/database";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDCBY31T7EkEkpuPCOfbJvnjZhED1Q7ciQ",
+  authDomain: "dustygalindo-23680.firebaseapp.com",
+  projectId: "dustygalindo-23680",
+  storageBucket: "dustygalindo-23680.appspot.com",
+  messagingSenderId: "511115371107",
+  appId: "1:511115371107:web:6a1e962419d31bec9a0e56",
+  measurementId: "G-TB8HRQ45N7",
+  // disable analytics
+  autoanalyticsEnabled: false
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// get database and export
+export const db = getDatabase(app);
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
--- pages/projects/job-search-log.tsx
+++ pages/projects/job-search-log.tsx
@@ -2,8 +2,28 @@ import Nav from 'src/components/nav'
 import Head from 'next/head'
 // import styles
 import styles from 'src/styles/JobSearchLog.module.css'
+import { useEffect, useState } from 'react';
+// import firebase
+import { ref, onValue } from "firebase/database";
+//import db from _app.tsx
+import { db } from 'src/pages/_app'
 
 export default function JobSearchLog() {
+    // get data from firebase. need to set types. fixed in next line
+    const [jobData, setJobData] = useState<any[]>([]);
+    useEffect(() => {
+        // get data from firebase
+        const dbRef = ref(db, 'jobs');
+        onValue(dbRef, (snapshot) => {
+            const data = snapshot.val();
+            const jobData = [];
+            for (let key in data) {
+                jobData.push({ id: key, ...data[key] });
+            }
+            setJobData(jobData);
+        });
+    }, []);
+
     
     return (
         <>
@@ -28,17 +48,30 @@ export default function JobSearchLog() {
                 </tr>
             </thead>
             <tbody>
-                {/* get data from firebase */}
-                <tr>
+                
+                
+                {/* <tr>
                     <td>company name</td>
                     <td>job title</td>
                     <td>date applied</td>
                     <td>application status</td>
                     <td>notes</td>
-                </tr>
+                </tr> */}
+                {/* get data from firebase */}
+                {jobData.map((job) => (
+                    // add conditional class for rejected applications
+                    <tr key={job.id} className={job.applicationStatus === 'rejected' ? styles.rejected : ''}>
+                        <td>{job.companyName}</td>
+                        <td>{job.jobTitle}</td>
+                        <td>{job.dateApplied}</td>
+                        <td>{job.applicationStatus}</td>
+                        <td>{job.notes}</td>
+                    </tr>
+                ))}
                 
             </tbody>
         </table>
+        <p>*just sample data for today. Will update tomorrow 03/23</p>
 
         </main>
         </>
