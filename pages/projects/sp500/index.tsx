1cc22c4193b60321455bb79a150faaa1b3d2e014
Dusty <dustygalindo@gmail.com>
2024-12-07 10:56:21 -0800

    minor update. it's working

--- pages/projects/sp500/index.tsx
+++ pages/projects/sp500/index.tsx
@@ -6,7 +6,7 @@ export default function SP500HomePage() {
   return (
     <div style={{ padding: '2rem' }}>
       <h1>S&P 500 Portfolio Builder</h1>
-      <p>Welcome! This app helps you build a stock portfolio by comparing pairs of stocks. Your final portfolio will be weighted by an Elo rating system based on your choices.</p>
+      <p>This app helps you build a stock portfolio by comparing pairs of stocks. Your final portfolio will be weighted by an Elo rating system based on your choices.</p>
       <Link href="/projects/sp500/compare">
         <button>Start</button>
       </Link>
