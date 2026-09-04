a0b7e78e6cb27dbb4e088925da25ad16c2b2cd49
Dusty <dustygalindo@gmail.com>
2026-09-03 21:52:46 -0700

    removing projects page from nav

--- components/nav.tsx
+++ components/nav.tsx
@@ -12,7 +12,6 @@ export default function Nav() {
         <ul className={styles.nav}>
           <Link href="/"><li className={router.pathname != '/' ? styles.inactive : styles.active}>Home</li></Link>
           <Link href="/about"><li className={router.pathname != '/about' ? styles.inactive : styles.active}>About</li></Link>
-          <Link href="/projects"><li className={router.pathname != '/projects' ? styles.inactive : styles.active}>Projects</li></Link>
           <Link href="/blog"><li className={router.pathname != '/blog' ? styles.inactive : styles.active}>Blog</li></Link>
           {/* <Link href="/contact"><li className={router.pathname != '/contact' ? styles.inactive : styles.active}>Contact</li></Link> */}
         </ul>
--- pages/projects/click.tsx
+++ pages/projects/click.tsx
@@ -1,14 +1,9 @@
-import Link from "next/link";
 import ClickCounter from "src/components/ClickCounter";
 
 export default function ClickPage() {
   return (
     <div style={{ display: "grid", placeItems: "center", minHeight: "60vh", gap: 12 }}>
       <ClickCounter />
-      <p>
-        More info here: <Link href="/blog/click-counter">Building a secure Click Counter</Link>
-      </p>
     </div>
   );
 }
-
